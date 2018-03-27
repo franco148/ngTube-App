ae9c6b61c8019f7b387f0f83bb82162956ddaa92
franco-extreme <franck_ral@hotmail.com>
2018-03-27 18:50:51 -0400

    ngTube-AppV1: Getting started - get playlist from a youtube channel.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { YoutubeService } from '../../services/youtube.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  videos:any[] = [];
+
+  constructor(public _youtubeService: YoutubeService) {
+
+    this._youtubeService.getVideos()
+                        .subscribe( videos => {
+                          console.log(videos);
+                          this.videos = videos;
+                        });
+  }
 
   ngOnInit() {
   }
--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -1,8 +1,42 @@
 import { Injectable } from '@angular/core';
+import { Http, URLSearchParams } from '@angular/http';
+import 'rxjs/Rx';
 
 @Injectable()
 export class YoutubeService {
 
-  constructor() { }
+  private youtubeUrl:string="https://www.googleapis.com/youtube/v3";
+  private apikey:string = "AIzaSyDQ00AmKIXo9OJpun4Dd3NzKF9zZVV5KtA";
+  private playlistId:string = "UUuaPTYj15JSkETGnEseaFFg";
+
+  private nextPageToken:string = "";
+
+  constructor(public http:Http) { }
+
+
+  getVideos() {
+    let url = `${ this.youtubeUrl }/playlistItems`;
+    let params = new URLSearchParams();
+
+    params.set('part', 'snippet');
+    params.set('maxResults', '10');
+    params.set('playlistId', this.playlistId);
+    params.set('key', this.apikey);
+
+    return this.http.get(url, { search: params })
+               .map(response => {
+                 // console.log(response.json());
+                 this.nextPageToken = response.json().nextPageToken;
+
+                 let videos:any[]=[];
+                 for (let video of response.json().items) {
+                     let snippet = video.snippet;
+
+                     videos.push(snippet);
+                 }
+
+                 return videos;
+               });
+  }
 
 }
